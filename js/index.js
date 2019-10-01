ed2c7dd9f440868fdabf17008bb402c5a4d0da90
Nathaniel Mosco <natemosco@gmail.com>
2019-10-01 17:09:41 -0400

    2 event listeners added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const images = document.querySelectorAll("img")
+images.forEach(image => {
+    image.addEventListener("click", (event) => {
+        image.style.transform = "rotate(720deg)"
+        image.style.transitionDuration = "2s"
+        event.stopPropagation()
+    })
+    image.addEventListener("dblclick", (event) => {
+        image.style.transform = "skew(190deg, 10deg)"
+        image.style.transitionDuration = "1s"
+        event.stopPropagation()
+    })
+});
+
